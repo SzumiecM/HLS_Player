fa921201b2e03582c72f2b78c4d2c1ce6b6d8ebd
SzumiecM <milosz.sz.m@gmail.com>
2021-05-27 17:40:57 +0200

    add choosing existing playlist

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+from os import walk
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'paprask asfaksfbasbfh 15h21951 25hasf bfasjfa'
 
 
-@app.route("/")
-def hello_world():
-    return render_template('home.html', )
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    _, _, playlists = next(walk('playlists'))
+
+    if request.method == 'POST':
+        chosen = request.form.get('existing_playlist')
+    else:
+        chosen = playlists[-1]
+
+    return render_template('home.html', playlists=playlists, chosen=chosen)
 
 
 @app.route('/get/<path>/<file>')
