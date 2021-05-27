b7b73fcb01ad2019a316bf24b14419994358fbef
SzumiecM <milosz.sz.m@gmail.com>
2021-05-27 18:15:53 +0200

    add posting urls but not working due to cors

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ app.config['SECRET_KEY'] = 'paprask asfaksfbasbfh 15h21951 25hasf bfasjfa'
 @app.route('/', methods=['POST', 'GET'])
 def home():
     _, _, playlists = next(walk('playlists'))
+    url = None
 
     if request.method == 'POST':
         chosen = request.form.get('existing_playlist')
+        if not chosen:
+            url = request.form.get('url')
     else:
         chosen = playlists[-1]
 
-    return render_template('home.html', playlists=playlists, chosen=chosen)
+    print(url)
+
+    return render_template('home.html', playlists=playlists, chosen=chosen, url=url)
 
 
 @app.route('/get/<path>/<file>')
